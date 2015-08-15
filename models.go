9cba2a06f0a5ad31afc9bc7f12d406acc5381fab
devilock76 <ken@mack-z.com>
2015-08-15 00:48:10 -0400

    Added more models

--- models.go
+++ models.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"html/template"
+	"time"
 )
 
 type Page struct {
@@ -25,3 +26,66 @@ type JSONPost struct {
 }
 
 type JSONPosts []JSONPost
+
+type FlexPost struct {
+	ID			int
+	Cat			int
+	Title		string
+	Content		string
+	Meta		[]string
+	Public		int
+	Published	time.Time
+	FlexField	[]string
+	FlexValue	[]string
+}
+
+type FlexPosts []FlexPost
+
+type PubliCat struct {
+	ID			int
+	Name		string
+}
+
+type PubliMenu struct {
+	Title		string
+	URL			string
+	Order		int
+	Parent		int
+}
+
+type PubliMenus []PubliMenu
+
+type PubliMeta struct {
+	MetaText	string
+	ItemType	[]int
+	Item		[]int
+}
+
+type PubliMetas []PubliMeta
+
+type Publibrary struct {
+	Path		string
+	Name		string
+	Type		string
+}
+
+type PubliUser struct {
+	UserName	string
+	FullName	string
+	MailAddr	string
+	PassHash	string
+	PassSalt	string
+	UserGroup	string
+}
+
+type PubliGroup struct {
+	GroupName	string
+	GroupDesc	string
+	AccessCat	[]int
+	AccessPerm	[]int
+}
+
+type PubliSess struct {
+	SessKey		string
+	UserName	string
+}
