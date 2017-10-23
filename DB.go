315e750e4039db24673b74783b84537cde567e7b
mackenziek@nccommunitycolleges.edu <mackenziek@nccommunitycolleges.edu>
2017-10-22 20:17:55 -0400

    more indentation issues

--- DB.go
+++ DB.go
@@ -8,27 +8,27 @@ import (
 )
 
 func DBSavePost (p FlexPost) ([]byte, error) {
-	db, err := bolt.Open(CurrentConfig.DB, 0644, nil)
-	if err != nil {
-		panic(err)
-	}
-	defer db.Close()
-	
-	//p.ID = newKey
-	
-	newKey := []byte("")
-	
-	err = db.Update(func(tx *bolt.Tx) error {
+    db, err := bolt.Open(CurrentConfig.DB, 0644, nil)
+    if err != nil {
+	    panic(err)
+    }
+    defer db.Close()
+
+    //p.ID = newKey
+
+    newKey := []byte("")
+
+    err = db.Update(func(tx *bolt.Tx) error {
         bucket, err := tx.CreateBucketIfNotExists([]byte("FlexPosts"))
         if err != nil {
             return err
         }
 
-		//encode struct to json here
-		value := ""
-		
-		key, _ := bucket.NextSequence()
-		newKey := []byte(strconv.FormatUint(key, 64))
+	//encode struct to json here
+	value := ""
+
+	key, _ := bucket.NextSequence()
+	newKey := []byte(strconv.FormatUint(key, 64))
         if err != nil {
             return err
         }
